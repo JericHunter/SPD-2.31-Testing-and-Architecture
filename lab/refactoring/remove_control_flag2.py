02653c3389dc4bcf052913d04ae919941c77cfdb
JericHunter <51177847+JericHunter@users.noreply.github.com>
2021-02-20 15:46:55 +0000

    rem control flag

--- lab/refactoring/remove_control_flag2.py
+++ lab/refactoring/remove_control_flag2.py
@@ -2,13 +2,16 @@
 # Remove control flag
 # Reference: https://stackoverflow.com/a/10140333/81306
 # This code snippet reads up to the end of the file
+  
 n = 16
-with open('foobar.file', 'rb') as fp:
-    running = True
-    while running:
+file = 'foobar.file'
+
+def readfile(file, n):
+    with open(file, 'rb') as fp:
         chunk = fp.read(n)
         if chunk == '': # end of file, stop running.
-            running = False
-        else:
-            print(chunk)
+            return
+        print(chunk)
         # process(chunk)
+
+readfile(file, n)
