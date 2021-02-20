f076555fc4ff1d89605e48ce30b2cb9e0386d80b
JericHunter <51177847+JericHunter@users.noreply.github.com>
2021-02-20 15:41:40 +0000

    decom n consol

--- lab/refactoring/consolidate_conditional.py
+++ lab/refactoring/consolidate_conditional.py
@@ -1,32 +1,41 @@
 # by Kami Bigdely
 # Consolidate conditional expressions
-def dice(ingredients):
-    print("diced all ingredients.")
-def mix_all(diced_ingredients):
-    print("mixed all.")
-def add_salt():
-    print('added salt.')
-def pour(liquid):
-    print('poured', liquid + '.',)
+class Recipe:
+    def __init__(self, ingredients):
+        self.__ingredients = tuple(ingredients)
 
-def make_shirazi_salad(ingredients):
-    if 'cucumber' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'tomato' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'onion' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'lemon juice' not in ingredients:
-        print('lacks ingredients.')
-        return
-    dice(ingredients)
-    mix_all(ingredients)
-    add_salt()
-    pour('lemon juice')
-    print('Your yummy shirazi salad is ready!')
+    @property
+    def ingredients(self):
+        return self.__ingredients
 
-if __name__ == "__main__":
-    make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
+    def dice(self, ingredients):
+        print("diced all ingredients.")
+    def mix_all(self, diced_ingredients):
+        print("mixed all.")
+    def add_salt(self):
+        print('added salt.')
+    def pour(self, liquid):
+        print('poured', liquid + '.',)
+
+class Shirazi(Recipe):
+    def __init__(self, ingredients):
+        super().__init__(ingredients)
+
+    def check_ingredients(self, user_ingredients):
+        return set(user_ingredients) == set(self.ingredients)
+
+    def prep_salad(self):
+        self.dice(self.ingredients)
+        self.mix_all(self.ingredients)
+        self.add_salt()
+        self.pour('lemon juice')
+        print('Your yummy shirazi salad is ready!')
+
+    def make_salad(self, user_ingredients):
+        if not self.check_ingredients(user_ingredients):
+            print('lacks ingredients.')
+            return
+        self.prep_salad()
+
+shirazi = Shirazi(['cucumber', 'tomato', 'lemon juice', 'onion'])
+print(shirazi.make_salad(['cucumber', 'lemon juice', 'tomato']))
--- lab/refactoring/decompose_conditional2.py
+++ lab/refactoring/decompose_conditional2.py
@@ -4,24 +4,70 @@
 
 # Blood test analysis program
 total_cholostrol = 70
-ldl = 30
-triglyceride = 120
+class Levels:
+    def __init__(self, good, borderline, high):
+        self.good = good
+        self.borderline = borderline
+        self.high = high
 
-if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
-    # good level
-    print('*** Good level of cholestrol ***')
-elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
-    # High cholestrol level
-    print('*** High cholestrol level ***')
-    print('start taking pills such as statins')
-    print('start TLC diet')
-elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
-    #TLC_diet
-    print('*** Borderline to moderately elevated ***')
-    print("Start TLC diet")
-    print("Under this meal plan, only 7 percent of your daily calories \nshould come from saturated fat.")
-    print('Some foods help your digestive tract absorb less cholesterol. For example,\nyour doctor may encourage you to eat more:')
-    print('oats, barley, and other whole grains.')
-    print('fruits such as apples, pears, bananas, and oranges.')
-else:
-    print('Error: unhandled case.')
+    def is_high(self, result):
+        if result >= self.high:
+            return True
+        return False
+
+    def is_good(self, result):
+        if result < self.good:
+            return True
+        return False
+
+    def is_borderline(self, result):
+        if result in self.borderline:
+            return True
+        return False
+
+TOTAL_CHOLESTEROL = Levels(200, range(200, 240), 240)
+LDL = Levels(100, range(130, 160), 160)
+TRIGLYCERIDES = Levels(150, range(150, 200), 200)
+class Bloodwork:
+
+    def __init__(self, total_cholesterol, ldl, triglycerides):
+        self.tc = total_cholesterol
+        self.ldl = ldl
+        self.trig = triglycerides
+
+    def levels_high(self):
+        return TOTAL_CHOLESTEROL.is_high(self.tc) or LDL.is_high(self.ldl) or TRIGLYCERIDES.is_high(self.trig)
+
+    def levels_good(self):
+        return TOTAL_CHOLESTEROL.is_good(self.tc) and LDL.is_good(self.ldl) and TRIGLYCERIDES.is_good(self.trig)
+
+    def levels_borderline(self):
+        return TOTAL_CHOLESTEROL.is_borderline(self.tc) or LDL.is_borderline(self.ldl) or TRIGLYCERIDES.is_borderline(self.trig)
+
+    def show_TLC_diet(self):
+        print("\nStart TLC diet")
+        print(" - Under this meal plan, only 7 percent of your daily calories \nshould come from saturated fat.")
+        print(' - Some foods help your digestive tract absorb less cholesterol. For example,\nyour doctor may encourage you to eat more:')
+        print(' - oats, barley, and other whole grains.')
+        print(' - fruits such as apples, pears, bananas, and oranges.')
+
+    def summary(self):
+        # High cholestrol level
+        if self.levels_high():
+            print('\n*** High cholestrol level ***')
+            print('start taking pills such as statins')
+            self.show_TLC_diet()
+        # good level
+        elif self.levels_good():
+            print('\n*** Good level of cholestrol ***')
+        # Borderline
+        elif self.levels_borderline():
+            print('\n*** Borderline to moderately elevated ***')
+            self.show_TLC_diet()
+        else:
+            print('Error: unhandled case.')
+
+tc = 70
+ldl = 30
+trig = 160
+Bloodwork(tc, ldl, trig).summary()
